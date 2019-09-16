4f05fac74c0f782b90791dfa6a2ac971dcbe6e17
Riko Otsuka <riko@bread-is-pan.local>
2019-09-16 23:08:12 +0900

    変更

---
+++ sketch2.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <script src="sketch.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/p5.js"></script>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+<style type="text/css">
+    .paintArea {
+        margin: 100px 100px;
+        width: 300px;
+        height: 300px;
+        border: 1px solid #AAA;
+    }
+</style>
+</head>
+<body>
+
+</body>
+</html>
